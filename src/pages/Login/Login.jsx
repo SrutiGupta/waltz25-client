f81abf2cee811c3ea2618fb665418f0ef37abcb6
Souvik <souvik13109@gmail.com>
2025-03-22 16:26:56 +0530

    login setup

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -11,16 +11,16 @@ const Login = () => {
 
   return (
     <div
-      className="min-h-screen flex items-center justify-end bg-cover bg-center"
-      style={{ backgroundImage: "url('/bg.png')" }}
+      className="min-h-screen flex items-center justify-center bg-cover bg-center"
+      // style={{ backgroundImage: "url('/bg.png')" }}
     >
-      <div className="w-96 mr-40">
+      <div className="w-96 ">
         <h2 className="text-6xl font-bold text-center mb-20 text-red-600">Sign In</h2>
         <form onSubmit={handleSubmit} className="space-y-8">
           <input
             type="email"
             placeholder="Email"
-            className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
@@ -28,14 +28,14 @@ const Login = () => {
           <input
             type="password"
             placeholder="Password"
-            className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
           />
           <button
             type="submit"
-            className="w-1/2 ml-24 bg-red-600  text-white p-3 rounded-lg hover:bg-blue-700 transition"
+            className="w-1/2 ml-24 bg-red-600  text-white p-3 rounded-lg hover:bg-red-900 transition"
           >
             Sign In
           </button>
