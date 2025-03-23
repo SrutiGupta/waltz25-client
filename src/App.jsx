dc81b1ec019956a416b7b802a9fe819508787091
SrutiGupta <mg6156710@gmail.com>
2025-03-23 15:21:44 +0530

    footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Signup from "./pages/Signup/Signup.jsx";
-import Login from "./pages/Login/Login.jsx";
-import Team from "./pages/Team/Team.jsx"
-import Merchandise from "./pages/Merchandise/Merchandise.jsx"
-import Schedule from "./pages/Schedule/Schedule.jsx"
-import Conditions from "./pages/Conditions/Conditions.jsx"
-const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/team" element={<Team />} />
-      <Route path="/merchandise" element={<Merchandise />} />
-      <Route path="/schedule" element={<Schedule />} />
-      <Route path="/terms" element={<Conditions />} />
-    </Routes>
-  </Router>
-);
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer"; // Make sure the path is correct
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        {/* Other Components (like Navbar, Home, etc.) can be added here */}
+        
+        {/* Main Content */}
+        <Routes>
+          <Route path="/" element={<h1>Welcome to Waltz 2025</h1>} />
+          {/* Add other routes here if needed */}
+        </Routes>
+
+        {/* Footer Component */}
+        <Footer />
+      </div>
+    </Router>
+  );
+}
 
 export default App;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
+import { faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <div className="footer-wrapper">
+            <div className="footer">
+                <div className="footer-top">
+                    <div className="footer-left">
+                        <h1>WALTZ <span className="highlight">2025</span></h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis atque consectetur doloribus? Obcaecati, quisquam quia dicta officiis temporibus soluta! Animi?</p>
+                    </div>
+                    <div className="footer-right">
+                        <div className="footer-links">
+                            <ul>
+                                <li><Link to="#">About us</Link></li>
+                                <li><Link to="#">FAQs</Link></li>
+                                <li><Link to="#">SPP</Link></li>
+                                <li><Link to="#">About Theme</Link></li>
+                            </ul>
+                        </div>
+                        <div className="footer-links">
+                            <ul>
+                                <li><Link to="#">Workshops</Link></li>
+                                <li><Link to="#">Exhibitions</Link></li>
+                                <li><Link to="#">Initiatives</Link></li>
+                                <li><Link to="#" className="highlight-link">Guest Lectures</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-line"></div>
+                <div className="footer-bottom">
+                    <div className="footer-contact">
+                        <p>Connect with us on:</p>
+                        <div className="footer-icons">
+                            <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
+                            <a href="#"><FontAwesomeIcon icon={faFacebook} /></a>
+                            <a href="#"><FontAwesomeIcon icon={faLinkedin} /></a>
+                        </div>
+                        <p>&copy; WALTZ 2025, All rights reserved</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Footer
+export default Footer;
